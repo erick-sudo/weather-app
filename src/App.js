1b27a063f97e0d68f32edde9d1b684a318eac9a3
Erick Ochieng <erickochieng766@gmail.com>
2023-01-24 20:34:30 +0300

    Navigationbar, Statistics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React from "react";
+import { Routes, Route} from "react-router-dom"
 import './App.css';
-import Current from "./components/Current"
+import Home from "./Home"
+import Current from "./Current"
+import Statistics from "./Statistics"
+import About from "./About"
+import Navigationbar from "./Navigationbar"
 
 function App() {
   return (
-    <div className="App">
-      <Current />
-    </div>
+    <>
+      <Navigationbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/current" element={<Current />} />
+        <Route path="/statistics" element={<Statistics />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
   );
 }
 
